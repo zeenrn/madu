bd6bbab61d49950096de35d00294789db6d06480
zeenrn <151641896+zeenrn@users.noreply.github.com>
2023-11-21 20:24:34 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-onload = () =>{
-        document.body.classList.remove("container");
-};
+onload = () => {
+        const c = setTimeout(() => {
+          document.body.classList.remove("not-loaded");
+          clearTimeout(c);
+        }, 1000);
+      };
+      
